f747ec6ed890b0c723682493dae57f89173238a8
freshposh-code <atomisefarouk919@gmail.com>
2024-01-07 01:17:48 +0100

    first commit

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -3,10 +3,11 @@ import { main } from '../assets'
 import { Styles } from '../Utils/Style';
 import { TransingText } from '../components/CustomText';
 import {motion} from 'framer-motion'
-import { slideIn, zoomIn } from '../Utils/motion';
-import toast from 'react-hot-toast';
+import { slideIn, textVariant, zoomIn } from '../Utils/motion';
 import SectionWrapper from '../HOC/SectionWrapper';
 import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
+import { Twitter, WhatsApp } from '@mui/icons-material';
 
 const Contact = () => {
     const formRef = useRef();
@@ -60,39 +61,50 @@ const Contact = () => {
             setLoading(false);
             console.error(error);
   
-            toast.error("Ahh, something went wrong. Please try again.")
+            toast.error("something went wrong. Please try again.")
           }
         );
     }
 
   return (
-    <section className={`${Styles.paddingX} ${Styles.paddingY} bg-white`}>
+    <section className={`${Styles.paddingX} ${Styles.paddingY} bg-white dark:bg-gray-800 sm:rounded-t-[50%] rounded-t-[180px]`}>
 
-<div className="flex justify-center items-center sm:mt-4 mt-16">
+<div className="flex justify-center items-center sm:mt-10 mt-16">
         <TransingText title='Get In Touch ' textStyles='flex justify-center items-baseline sm:text-4xl xm:text-3xl text-2xl absolute' />
         
         <div className="flex justify-center items-center">
-        <motion.h1 variants={zoomIn(0.70, 1)} className='sm:text-7xl xm:text-6xl text-4xl -z-0 flex justify-center absolute text-[#51515143]'>CONTACT</motion.h1>
+        <motion.h1 variants={zoomIn(0.70, 1)} className='sm:text-7xl xm:text-[55px] text-4xl -z-0 flex justify-center absolute text-[#51515143]'>CONTACT</motion.h1>
         </div>
         </div>
 
-        <motion.h1 className='sm:text-lg text-base font-bold text-center mt-5 py-5 leading-7 dark:text-white'>Feel free to reach out for collaborations, Freelance, have inquiries?
+        <motion.h1 variants={textVariant(0.2)} className='sm:text-lg text-base font-bold text-center mt-5 py-8 leading-7 dark:text-white max-w-[1000px] m-auto'>Feel free to reach out for collaborations, Freelance, have inquiries?
          I'm always open to conversations! Your messages and feedback are valued, and I look forward to engaging with you. Feel free to get in touch  with me across various social media platforms.
          or just send me a message right away 👇.</motion.h1>
 
-        <div className="flex justify-between sm:flex-row flex-col-reverse gap-6 overflow-x-hidden">
+        <div className="flex justify-between items-center sm:flex-row flex-col-reverse gap-6 overflow-x-hidden">
 
         <motion.div variants={slideIn('left', 'tween', 0.5, 1)}>
-            <img src={main} alt="image" className='object-cover w-[600px] sm:h-[600px] h-[450px]' />
+            <img src={main} alt="image" className='object-cover w-[100%] sm:h-[600px] h-[450px]' />
         </motion.div>
 
-        <motion.div variants={slideIn('right', 'tween', 0.5, 1.4)} className='bg-[#000] sm:p-3 p-2 rounded-2xl sm:w-[500px] w-[100%]'>
+        <motion.div variants={slideIn('right', 'tween', 0.0, 1.4)} className='bg-[#000] sm:p-3 p-2 rounded-2xl sm:w-[500px] w-[100%] m-auto mt-6'>
             
              <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8 rounded-lg sm:p-3 xm:p-3 p-0'
+          className='flex flex-col gap-6 rounded-lg sm:p-3 xm:p-3 p-0'
         >
+            <div className='flex items-center justify-around text-white font-extrabold'>
+            <h1 className=''>VIA</h1>
+            <a href="https://wa.me/+2349068620646?text=Hi%20there👋" target='_blank'>
+            <WhatsApp style={{color: 'white', backgroundColor: 'green', padding: '6px', borderRadius:'50%', fontSize:'29px'}} className="icon" />
+            </a>
+            OR
+            <a href="https://twitter.com/farouk_ayo" target='_blank'>
+            <Twitter style={{backgroundColor: '#4a6fff', color: 'white', padding: '6px', borderRadius:'50%', fontSize:'29px'}} className="icon" />
+            </a>
+            </div>
+            <h1 className="text-white font-bold sm:text-2xl text-lg">📧Connect With Me👇</h1>
           <label className='flex flex-col'>
             <span className='text-blue-200 mb-4 font-bold'>Your Name</span>
             <input
@@ -123,7 +135,7 @@ const Contact = () => {
               value={form.message}
               onChange={handleChange}
               placeholder='What you want to say?'
-              className='py-1 px-6 placeholder:text-secondary rounded-lg outline-none border-none font-medium poppins placeholder:pt-3'
+            className='py-1 px-6 placeholder:text-secondary rounded-lg outline-none border-none font-medium poppins'
             />
           </label>
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>
+  <BrowserRouter>
   <Toaster position='top-center' reverseOrder={false}/>
     <App />
-  </div>
+  </BrowserRouter>,
 )
